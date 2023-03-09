aadcf61641a60fcdbe1c74cff9e42de194b3576d
adesoba5@gmail.com <adesoba5@gmail.com>
2023-03-09 08:19:10 +0000

    update core, intermediate

---
+++ models/mart/core/dim_customer.sql
@@ -0,0 +1,25 @@
+
+
+{{
+  config(
+    materialized = 'table'
+  )
+}}
+
+with users as (
+
+  select 
+    * 
+  from {{ ref('int_sessions_customer__grouped') }}
+
+),
+
+final as (
+
+  select
+    *
+  from users
+
+)
+
+select * from final
